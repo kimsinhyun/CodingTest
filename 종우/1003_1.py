98aff32b03b3aaf9f49bf816e3e82113aa634caa
user <jongwoop97@gmail.com>
2022-10-04 01:11:08 +0900

    finished 3 problems

--- 종우/1003_1.py
+++ 종우/1003_1.py
@@ -1,6 +1,7 @@
 # 최댓값과 최솟값
-# https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=python3
+# https://school.programme  rs.co.kr/learn/courses/30/lessons/12939?language=python3
 
 def solution(s):
-    answer = ''
+    toarr = [int(x) for x in s.split()]
+    answer = str(min(toarr)) + " " + str(max(toarr))
     return answer
---
+++ 종우/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+tmp = ["696969", "666", "606060"]
+tmp2 = ["656565", "666", "606060"]
+tmp.sort(reverse=True)
+tmp2.sort(reverse=True)
+print(tmp)
+print(tmp2)
