414095df7433843b0b7d58f7e00ac8678bed0fbf
user <jongwoop97@gmail.com>
2022-10-04 23:52:44 +0900

    10/4 p4 done

--- 종우/tempCodeRunnerFile.py
+++ 종우/tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
-tmp = ["696969", "666", "606060"]
-tmp2 = ["656565", "666", "606060"]
-tmp.sort(reverse=True)
-tmp2.sort(reverse=True)
-print(tmp)
-print(tmp2)
+import math
+
+n = int(input())
+print(math.factorial(n))
