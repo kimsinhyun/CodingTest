c94ef6390e76cd4576a2cf06ca161a6f338afcde
user <jongwoop97@gmail.com>
2023-01-19 00:46:31 +0900

    0118 dfs sol

--- 종우/타겟넘버.py
+++ 종우/타겟넘버.py
@@ -1,6 +1,8 @@
 # 0118_1
 # https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
+
+# 내 풀이 178
 def solution(numbers, target):
     answer = 0
     sums = [numbers[0], -numbers[0]]
@@ -12,3 +14,21 @@ def solution(numbers, target):
             tmp.append(j-i)
         sums = tmp
     return tmp.count(target)
+
+# dfs 풀이
+answer = 0
+def DFS(idx, numbers, target, value):
+    global answer
+    N = len(numbers)
+    if(idx== N and target == value):
+        answer += 1
+        return
+    if(idx == N):
+        return
+
+    DFS(idx+1,numbers,target,value+numbers[idx])
+    DFS(idx+1,numbers,target,value-numbers[idx])
+def solution(numbers, target):
+    global answer
+    DFS(0,numbers,target,0)
+    return answer
