d39c5df4170d72ccea717fbced840fefa618ca59
user <jongwoop97@gmail.com>
2023-02-03 19:01:32 +0900

    0203

--- 종우/등굣길.py
+++ 종우/등굣길.py
@@ -45,4 +45,18 @@ def solution(m, n, puddles):
     print(dp)
     return dp[-1][-1]
 
-# 
+# O(N)
+def solution(m, n, puddles):
+    puddles = [[a, b] for [b, a] in puddles]
+    dp = [[0]*(m+1) for i in range(n+1)]
+    dp[1][1] = 1
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1 and j == 1:
+                continue
+            elif [i,j] in puddles:
+                dp[i][j] = 0
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    return dp[n][m]%1000000007
