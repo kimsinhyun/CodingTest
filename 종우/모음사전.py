6b21d69f596c79b0e41cdb00b9aacf23117cd54e
user <jongwoop97@gmail.com>
2023-02-10 12:41:30 +0900

    0210_1

--- 종우/모음사전.py
+++ 종우/모음사전.py
@@ -1,6 +1,15 @@
 # 0209
 # https://school.programmers.co.kr/learn/courses/30/lessons/84512
 
+# itertools product 중복 순열
+from itertools import product
 def solution(word):
     answer = 0
+    words = []
+    for i in range(1, 6):
+        c = product(['A','E','I','O','U'], repeat=i)
+        for comb in c:
+            words.append(''.join(comb))
+    words.sort()
+    answer = words.index(word) +1
     return answer
