f32de713983cec3ecad68b83caf2fad406e02798
user <jongwoop97@gmail.com>
2023-02-22 22:02:09 +0900

    0222

---
+++ 종우/같은숫자는싫어.py
@@ -0,0 +1,10 @@
+# 0222
+# https://school.programmers.co.kr/learn/courses/30/lessons/12906
+
+# 스택큐
+
+def solution(arr):
+    answer = []
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+    return answer
