5cc24df32725bae18e9ce11399843214a3db04ae
user <jongwoop97@gmail.com>
2023-03-12 23:36:33 +0900

    0312

---
+++ 종우/디스크컨트로럴러.py
@@ -0,0 +1,10 @@
+# 0312
+# https://school.programmers.co.kr/learn/courses/30/lessons/42627
+
+import heapq
+
+def solution(jobs):
+    answer = 0
+    heapq.heapify(jobs)
+    print(jobs)
+    return answer
