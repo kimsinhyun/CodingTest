d79e0f3345d5218a49b7070ca3391fd2f9820474
user <jongwoop97@gmail.com>
2023-03-15 23:17:51 +0900

    0315

--- 종우/디스크컨트로럴러.py
+++ 종우/디스크컨트로럴러.py
@@ -2,9 +2,21 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42627
 
 import heapq
-
 def solution(jobs):
     answer = 0
-    heapq.heapify(jobs)
-    print(jobs)
-    return answer
+    time = 0
+    heap = []
+    length = len(jobs)
+    jobs.sort()
+
+    while len(heap) != 0 or len(jobs) != 0:
+        while len(jobs) != 0 and jobs[0][0] <= time:
+            heapq.heappush(heap, jobs.pop(0)[::-1])
+        if len(heap)==0:
+            time = jobs[0][0]
+            continue
+        process = heapq.heappop(heap)
+        time += process[0]
+        answer += time - process[1]
+
+    return answer//length
