713f956bfa461a13942d9dde1d523e9d3d7257b4
user <jongwoop97@gmail.com>
2023-03-26 19:41:47 +0900

    0326

--- 종우/여행경로.py
+++ 종우/여행경로.py
@@ -31,4 +31,35 @@ def dfs(flights, n, path):
         path.append(nx)
         dfs(flights, n, path)
         
+# dfs solution
+import collections
+answer = []
+graph = collections.defaultdict(list)
+visited = collections.defaultdict(list)
+
+def solution(tickets):
+    for a,b in tickets:
+        graph[a].append(b)
+        visited[a].append(0)
+    for a,b in graph.items():
+        graph[a].sort()
+    
+    dfs('ICN', ['ICN'], 0, len(tickets))
+    
+    return answer
+
+def dfs(cur, path, cnt, k):
+    if cnt == k:
+        global answer
+        answer = path
+        return
+    
+    if len(answer) >= 1:
+        return
+    
+    for i in range(len(graph[cur])):
+        if not visited[cur][i]:
+            visited[cur][i] = 1
+            dfs(graph[cur][i], path + [graph[cur][i]], cnt+1, k)
+            visited[cur][i] = 0
         
