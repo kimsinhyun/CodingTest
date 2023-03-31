fcaad0118c20fb9604b1e49ffe5a45d7fdd88f29
user <jongwoop97@gmail.com>
2023-03-31 18:40:27 +0900

    0331_1

--- 종우/bj/14502/script.py
+++ 종우/bj/14502/script.py
@@ -2,23 +2,31 @@
 # 0330
 # https://www.acmicpc.net/problem/14502
 
-
 import sys
 
-# 표준 입력을 파일로 설정하기 -> input.txt를 읽어들여옴
 sys.stdin = open("test1.txt", "r")
-
-# 표준 입력을 파일로 바꿨으므로, 
-# input() 함수는 더이상 사용자 입력을 받지 않고, 지정된 입력 파일을 읽게 됨.
 n,m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-# list comprehension을 이용해 이후의 입력값을 받아옴
-arr = [list(map(int, input().split())) for _ in range(m)]
+"""
+simulate expansion
+count safe zones
+"""
+
+infected = True
+while infected:
+    infected = False
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == 2 and i<0:
+                pass
+                
+
+
 print(arr)
 
-
-# visited = [[False]*m for _ in range(n)]
-
-# n, m = map(int, input().split())
-# board = [list(map(int, input.split())) for _ in range(n)]
-# print(board)
+def countSafe(arr):
+    safezones = 0
+    for s in arr:
+        safezones += s.count(0)
+    return safezones
--- 종우/bj/연구소.py
+++
@@ -1,8 +0,0 @@
-# 0330
-# https://www.acmicpc.net/problem/14502
-
-# visited = [[False]*m for _ in range(n)]
-
-n, m = map(int, input().split())
-board = [list(map(int, input.split())) for _ in range(n)]
-print(board)
