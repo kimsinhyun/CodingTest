74034057ffd0a3d387802728588c65a537472dcc
user <jongwoop97@gmail.com>
2023-03-31 15:08:26 +0900

    0331

--- 종우/백준/연구소.py
+++ 종우/백준/연구소.py
@@ -1,7 +1,7 @@
 # 0330
 # https://www.acmicpc.net/problem/14502
 
-visited = [[False]*m for _ in range(n)]
+# visited = [[False]*m for _ in range(n)]
 
 n, m = map(int, input().split())
 board = [list(map(int, input.split())) for _ in range(n)]
