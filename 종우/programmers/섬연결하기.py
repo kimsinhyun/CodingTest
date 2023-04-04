5dc6445d8ac7448da689efd5df2c4f7400a13269
user <jongwoop97@gmail.com>
2023-04-04 21:01:55 +0900

    0404

--- 종우/programmers/섬연결하기.py
+++ 종우/programmers/섬연결하기.py
@@ -15,3 +15,39 @@ def solution(n, costs):
     
     return answer
 
+# solve using kruskals    
+def getParent(parent, a):
+    if parent[a] == a:
+        return a
+    return getParent(parent, parent[a])
+
+def unionParent(parent, a, b):
+    a = getParent(parent, a)
+    b = getParent(parent, b)
+
+    if parent[a] < parent[b]:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def findParent(parent, a, b):
+    if getParent(parent, a) == getParent(parent, b):
+        return True
+    return False
+
+def solution(n, costs):
+    answer = 0
+    costs.sort(key = lambda x:x[2])
+    parent = [i for i in range(n)]
+
+    count = 0
+    for cost in costs:
+        if count == n-1:
+            break
+        
+        if not findParent(parent, cost[0], cost[1]):
+            unionParent(parent, cost[0], cost[1])
+            answer += cost[2]
+            count += 1
+
+    return answer
