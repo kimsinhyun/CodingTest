9738777c04e34766390951db88abf50611d2122d
user <jongwoop97@gmail.com>
2023-04-06 12:53:23 +0900

    0406_1

--- 종우/programmers/순위.py
+++ 종우/programmers/순위.py
@@ -1,7 +1,7 @@
 # 0405
 # https://school.programmers.co.kr/learn/courses/30/lessons/49191
 
-# initial solve using floyd-warshall
+# solve using floyd-warshall
 def solution(n, results):
     answer = 0
     floyd = [[0] * n for i in range(n)]
@@ -17,6 +17,9 @@ def solution(n, results):
                 if floyd[i][k] == 1 and floyd[k][j] == 1:
                     floyd[i][j] = 1
                     floyd[j][i] = -1
+                if floyd[j][k] ==1 and floyd[k][i] == 1:
+                    floyd[j][i] = 1
+                    floyd[i][j] = -1
     
     for r in floyd:
         if r.count(0) <= 1:
