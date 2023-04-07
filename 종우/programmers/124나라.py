92964d73c33b8c4aa042557e52d8cb667a538acf
user <jongwoop97@gmail.com>
2023-04-07 17:13:33 +0900

    0407_1

---
+++ 종우/programmers/124나라.py
@@ -0,0 +1,19 @@
+# 0407
+# https://school.programmers.co.kr/learn/courses/30/lessons/12899#
+
+
+def solution(n):
+    answer = ''
+    n -= 1
+    dig = ['1', '2', '4']
+    
+    if n < 3:
+        return dig[n]
+    
+    while n >= 3:
+        last = n%3
+        answer = dig[last] + answer
+        n = n//3
+        n -=1
+    
+    return answer
