2b79e8abdea752e8e5860490f7198939020b6c17
user <jongwoop97@gmail.com>
2023-04-26 23:18:14 +0900

    0426-2

--- 종우/programmers/롤케이크자르기.py
+++ 종우/programmers/롤케이크자르기.py
@@ -1,6 +1,34 @@
 # 0426
 # https://school.programmers.co.kr/learn/courses/30/lessons/132265
 
+# 시간초과
+def fair(l, r):
+    lc = 0
+    rc = 0
+    for t in l:
+        if l[t] > 0:
+            lc +=1
+        if r[t] > 0:
+            rc +=1
+    if lc == rc:
+        return True
+    return False
+
 def solution(topping):
-    answer = -1
+    answer = 0
+    l = {}
+    r = {}
+    
+    for t in topping:
+        if t not in l:
+            l[t] = 0
+            r[t] = 0
+        r[t] += 1
+    
+    for t in topping:
+        l[t] += 1
+        r[t] -= 1
+        if fair(l, r):
+            answer += 1
+    
     return answer
