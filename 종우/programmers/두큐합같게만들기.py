5c30da64702f650585781fc8552c93e798058336
jong29 <jongwoop97@gmail.com>
2023-05-06 03:18:34 +0900

    0505_2

--- 종우/programmers/두큐합같게만들기.py
+++ 종우/programmers/두큐합같게만들기.py
@@ -2,11 +2,28 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118667
 
 def solution(queue1, queue2):
-    answer = -2
+    answer = 0
     whole = queue1 + queue2
-    goal = sum(whole)//2
+    s1 = sum(queue1)
+    s2 = sum(queue2)
+    p1 = 0
+    p2 = len(queue1)    
+    goal = (s1+s2)//2
     
-    l = 0
-    r = len(queue1)
+    for i in range(p2*4):
+        if s1 > s2:
+            s1 -= whole[p1]
+            s2 += whole[p1]
+            p1 = (p1+1) % (len(queue1)*2)
+            answer += 1
+        elif s1 < s2:
+            s2 -= whole[p2]
+            s1 += whole[p2]
+            p2 = (p2+1) % (len(queue1)*2)
+            answer += 1
+        else:
+            break
+    else:
+        return -1
     
     return answer
