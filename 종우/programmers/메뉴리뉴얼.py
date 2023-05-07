a5810c9c4e7d722b0583518f261155c02bcde5ce
user <jongwoop97@gmail.com>
2023-05-07 23:06:36 +0900

    0507

--- 종우/programmers/메뉴리뉴얼.py
+++ 종우/programmers/메뉴리뉴얼.py
@@ -26,3 +26,30 @@ def solution(orders, course):
     answer.sort()
     
     return answer
+
+# 정답
+def solution_itertools(orders, course):
+        import itertools
+
+        answer = []
+
+        for size_of_course in course:
+            order_dict = {}
+            order_combinations = []
+            for order in orders:
+                order_combinations.extend(list(itertools.combinations(sorted(order), size_of_course)))
+
+            for order_combination in order_combinations:
+                order_str = ''.join(order_combination)
+                if order_str in order_dict:
+                    order_dict[order_str] += 1
+                else:
+                    order_dict[order_str] = 1
+
+            for order in order_dict:
+                if order_dict[order] == max([order_dict[order] for order in order_dict]):
+                    if order_dict[order] > 1:
+                        answer.append(order)
+
+
+        return sorted(answer)
