879cc7841d94b13744440f3f255f1ee5dc6d87e0
user <jongwoop97@gmail.com>
2023-05-09 21:01:51 +0900

    0509_2

--- 종우/programmers/보석쇼핑.py
+++ 종우/programmers/보석쇼핑.py
@@ -62,3 +62,31 @@ def solution(gems):
             answer = r
     
     return answer
+
+# better optimized
+def solution(gems):
+    n = len(set(gems))
+    counter = {gems[0]:1}
+    potential = [0, len(gems) -1]
+    start, end = 0, 0
+    
+    
+    while start < len(gems) and end < len(gems):
+        if len(counter) == n:
+            if end - start < potential[1] - potential[0]:
+                potential = [start, end]
+            if counter[gems[start]] == 1:
+                del counter[gems[start]]
+            else:
+                counter[gems[start]] -= 1
+            start += 1
+        else:
+            end += 1
+            if end == len(gems):
+                break
+            if gems[end] not in counter:
+                counter[gems[end]] = 1
+            else:
+                counter[gems[end]] += 1
+    
+    return [potential[0]+1, potential[1]+1]
