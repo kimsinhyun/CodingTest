0dca186d10a42d7579467ce94d64bef49a223e1b
jong29 <jongwoop97@gmail.com>
2023-05-25 15:17:57 +0900

    0525 valid palindrome

---
+++ 종우/leetcode/0525_125.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/valid-palindrome/
+
+
+# my solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        l = 0
+        r = len(s)-1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -=1
+                continue
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal: Panama"))
