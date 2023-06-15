07003ca0f376855f834fcea29cd95234fe805d82
user <jongwoop97@gmail.com>
2023-06-15 22:36:31 +0900

    0615_2

--- 종우/leetcode/0615_46.py
+++ 종우/leetcode/0615_46.py
@@ -21,3 +21,20 @@ class Solution:
         return res
     
 
+# solution using recursive backtracking...
+class Solution:
+    def permute(self, nums: list[int]) -> list[list[int]]:
+        res = []
+
+        if len(nums) == 1:
+            return [nums[:]]
+
+        for i in range(len(nums)):
+            tmp = nums.pop(0)
+            perms = self.permute(nums)
+            for perm in perms:
+                perm.append(tmp)
+            res.extend(perms)
+            nums.append(tmp)
+            
+        return res
