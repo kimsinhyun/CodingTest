7249528ff1c4daa7ac49d56dd0f02046eed548db
user <jongwoop97@gmail.com>
2023-06-29 00:50:20 +0900

    fixed solution such that visited known when appending to queue

--- 종우/leetcode/0628_200.py
+++ 종우/leetcode/0628_200.py
@@ -1,8 +1,9 @@
 # https://leetcode.com/problems/number-of-islands/
 
-# timeout solution
+from collections import deque
+
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid: list[list[str]]) -> int:
         m, n = len(grid), len(grid[0])
         visited = [[0]*n for i in range(m)]
         islands = 0
@@ -11,10 +12,11 @@ class Solution:
             for x in range(n):
                 if grid[y][x] == "1" and visited[y][x] == 0:
                     islands += 1
-                    q = [(x, y)]
+                    q = deque()
+                    q.append((x, y))
+                    visited[y][x] = 1
                     while q:
-                        cx, cy = q.pop(0)
-                        visited[cy][cx] = 1
+                        cx, cy = q.popleft()
                         dx = [-1, 1, 0, 0]
                         dy = [0, 0, -1, 1]
                         for i in range(4):
@@ -22,5 +24,6 @@ class Solution:
                             if nx >= 0 and nx < n and ny >= 0 and ny < m:
                                 if grid[ny][nx] == "1" and visited[ny][nx] == 0:
                                     q.append((nx, ny))
+                                    visited[ny][nx] = 1
         
         return islands
