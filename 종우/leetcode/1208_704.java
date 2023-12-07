ad62c35c73ed774a86ad2c81ed6362f95e087fed
user <jongwoop97@gmail.com>
2023-12-08 00:34:57 +0900

    231208 test

---
+++ 종우/leetcode/1208_704.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+      int n = nums.length;
+      int l = 0;
+      int r = n;
+
+      while (l < r) {
+        int m = (l + r) / 2;
+        if (nums[m] == target) return m;
+        else if (nums[m] > target) {
+          r = m; 
+        } else {
+          l = m+1;
+        }
+      }
+      return -1;
+    }
+}
