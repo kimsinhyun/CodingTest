59b3f682cce992c3622a53a64fd6dee94c2b1eba
박종우 <jongwoop97@gmail.com>
2023-12-19 16:08:09 +0900

    231219-2

--- 종우/leetcode/javapkg/Sol57.java
+++ 종우/leetcode/javapkg/Sol57.java
@@ -74,3 +74,32 @@ class Solution {
         return ans.toArray(new int[ans.size()][2]);
     }
 }
+
+// neetcode solution in java
+
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+        
+        boolean inserted = false;
+        for (int i=0; i<intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                ans.add(newInterval);
+                inserted = true;
+                for (int j=i; j<intervals.length; j++) {
+                    ans.add(intervals[j]);
+                }
+                break;
+            } else if (newInterval[0] > intervals[i][1]) {
+               ans.add(intervals[i]); 
+            } else {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            }
+        }
+        if (!inserted) {
+            ans.add(newInterval);
+        }
+        return ans.toArray(new int[ans.size()][2]);
+    }
+}
