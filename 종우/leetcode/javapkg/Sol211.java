8ffde2fd42b690b2acd429e860b5d56d244d68e6
박종우 <jongwoop97@gmail.com>
2024-01-08 23:13:48 +0900

    240108 fix

--- 종우/leetcode/javapkg/Sol211.java
+++ 종우/leetcode/javapkg/Sol211.java
@@ -17,24 +17,17 @@ class Sol211 {
     
     public void addWord(String word) {
         Map<Character, Node> cur = trie.nextChars;
+        Node cur = trie;
         for (int i=0; i<word.length(); i++) {
             char c = word.charAt(i);
-            if (cur.containsKey(c)) {
-                cur = cur.get(c).nextChars;
-            } else {
-                for (int j=i; j < word.length(); j++) {
-                    char nc = word.charAt(j);
-                    cur.put(nc, new Node());
-                    if (j == word.length() - 1) {
-                        cur.get(nc).isWord = true;
-                    }
-                    cur = cur.get(nc).nextChars;
-                }
-                break;
+            if (!cur.nextChars.containsKey(c)) {
+                cur.put(c, new Node());
             } 
+            cur = cur.get(c);
         }
+        cur.isWord = true;
     }
-    
+
     public boolean search(String word) {
         return dfs(word, 0, trie);
     }
@@ -42,25 +35,21 @@ class Sol211 {
     private boolean dfs(String word, int cnt, Node cur) {
         if (cnt == word.length()) {
             if (cur.isWord) return true;
-            else return false;
+            return false;
         }
+
         char c = word.charAt(cnt);
         if (c == '.') {
-            if (cur.nextChars.size() == 0) {
-                return false;
-            } else {
-                for (char nc: cur.nextChars.keySet()) {
-                    if(dfs(word, cnt+1, cur.nextChars.get(nc))){
-                        return true;
-                    }
+            for (char nc: cur.nextChars.keySet()) {
+                if(dfs(word, cnt+1, cur.nextChars.get(nc))){
+                    return true;
                 }
-                return false;
             }
-        } else if (!cur.nextChars.containsKey(c)) {
             return false;
-        } else {
+        } else if (!cur.nextChars.containsKey(c)) {
             return(dfs(word, cnt+1, cur.nextChars.get(c)));
         }
+        return false;
     }
 }
 
